69bae4dc3081b9e42f28a43965e66ce9bf44c5d5
Gavin Schriver <alexanderschriver@gmail.com>
2020-10-14 00:00:05 -0500

    listing  all entries

---
+++ entries/request.py
@@ -0,0 +1,23 @@
+import sqlite3
+import json
+from models import Entry
+
+def get_all_entries():
+    with sqlite3.connect("./dailyjournal.db") as con:
+        con.row_factory = sqlite3.Row
+        cursor = con.cursor()
+        cursor.execute("""
+        SELECT
+            e.id,
+            e.date,
+            e.topics,
+            e.entry,
+            e.moodId,
+            e.instructorId
+        FROM Entries e
+        """)
+        entries = []
+        data = cursor.fetchall()
+        for row in data:
+            entries.append(Entry(row['id'], row['date'], row['topics'], row['entry'], row['moodId'], row['instructorId']).__dict__)
+        return json.dumps(entries)
---
+++ models/entry.py
@@ -0,0 +1,8 @@
+class Entry:
+    def __init__(self, id, date, topics, entry, moodId, instructorId):
+        self.id = id
+        self.date = date
+        self.topics = topics
+        self.entry = entry
+        self.moodId = moodId
+        self.instructorId = instructorId
